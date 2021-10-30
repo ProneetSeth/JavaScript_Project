c7257b96389b19dba47a67a8188121d4e2357dcd
ProneetSeth <proneetofficial111@gmail.com>
2021-10-30 20:55:19 +0530

    Auto scroll on left while dragging and selecting

--- Excel_Clone/script.js
+++ Excel_Clone/script.js
@@ -1,5 +1,5 @@
 //adding perfect scrollbar
-const ps = new PerfectScrollbar('#cells',{
+const ps = new PerfectScrollbar('#cells', {
     wheelSpeed: 0.4,
     //wheelPropagation: true,
 });
@@ -45,61 +45,61 @@ for (let i = 1; i <= 100; i++) { // abhi 100 column k liye chala rahe hai
 //---------------------------------------------
 
 //(here i is the row number and j is the column)
-for(let i = 1; i <= 100; i++){
+for (let i = 1; i <= 100; i++) {
     let row = $('<div class = "cell-row"></div>') //appending row
-    for(let j = 1; j <= 100; j++){
+    for (let j = 1; j <= 100; j++) {
         row.append(`<div id = "row-${i}-col-${j}" class="input-cell" contenteditable = "false"></div>`) //jab double click ho tab editable ho
     }
-    $("#cells").append(row); 
+    $("#cells").append(row);
 }
 
 
 //scroll bar ke saath saath row and column ke value i.e A,B,C,D,........ and 1,2,3,4....... move karne ke liye
-$("#cells").scroll(function(e){
+$("#cells").scroll(function (e) {
     //jitna scroll cells karega utna hi scroll column kare
     $("#columns").scrollLeft(this.scrollLeft);
     $("#rows").scrollTop(this.scrollTop);
 })
 
 //double click pe content editable true kara hai and focus pe laya hai 
-$(".input-cell").dblclick(function(e){
+$(".input-cell").dblclick(function (e) {
     $(".input-cell.selected").removeClass("selected top-selected bottom-selected left-selected right-selected");
     //$(this).addClass("selected");
-    $(this).attr("contenteditable","true");
+    $(this).attr("contenteditable", "true");
     $(this).focus();
 })
 
 //jab particular cell se work khatam hojaye toh content editable false kardena hai 
-$(".input-cell").blur(function(e){
-    $(this).attr("contenteditable","false");
+$(".input-cell").blur(function (e) {
+    $(this).attr("contenteditable", "false");
 })
 
 //to select multiple column 
-function getRowCol(ele){
+function getRowCol(ele) {
     let id = $(ele).attr("id"); // while making cell , given an id as id row-rownum col-colnum(get the id of that element)
     let idArray = id.split("-"); //convert id into array
     let rowId = parseInt(idArray[1]);
     let colId = parseInt(idArray[3]);
-    return [rowId,colId]; //isko return kardiya 
+    return [rowId, colId]; //isko return kardiya 
 
 }
 
 //getting top,left,bottom,right cell
-function getTopLeftBottomRightCell(rowId,colId){
+function getTopLeftBottomRightCell(rowId, colId) {
     let topCell = $(`#row-${rowId - 1}-col-${colId}`);
     let bottomCell = $(`#row-${rowId + 1}-col-${colId}`);
     let leftCell = $(`#row-${rowId}-col-${colId - 1}`);
     let rightCell = $(`#row-${rowId}-col-${colId + 1}`);
-    return [topCell,bottomCell,leftCell,rightCell]; 
+    return [topCell, bottomCell, leftCell, rightCell];
 }
 
 
 //input cell ke single click par usko select karna hai. (Bit hard logic)
-$(".input-cell").click(function(e){
+$(".input-cell").click(function (e) {
     // $(".input-cell.selected").removeClass("selected"); //ek hi time par ek hi cell select karne ke liye 
     // $(this).addClass("selected"); // this code push to below logic
-    let [rowId,colId] = getRowCol(this); // yaha receive kiya & this is current element 
-    let [topCell,bottomCell,leftCell,rightCell] = getTopLeftBottomRightCell(rowId,colId);
+    let [rowId, colId] = getRowCol(this); // yaha receive kiya & this is current element 
+    let [topCell, bottomCell, leftCell, rightCell] = getTopLeftBottomRightCell(rowId, colId);
     if ($(this).hasClass("selected") && e.ctrlKey) {
         unselectCell(this, e, topCell, bottomCell, leftCell, rightCell); //unselect a particular cell after selecting multiple cell
     } else {
@@ -134,51 +134,51 @@ function unselectCell(ele, e, topCell, bottomCell, leftCell, rightCell) {
 
 
 
-function selectCell(ele,e,topCell,bottomCell,leftCell,rightCell){
-    if(e.ctrlKey){
+function selectCell(ele, e, topCell, bottomCell, leftCell, rightCell) {
+    if (e.ctrlKey) {
         //top cell selected or not
         let topSelected;
         console.log(topCell);
-        if(topCell){
+        if (topCell) {
             topSelected = topCell.hasClass("selected"); //agar topCell selected hai toh us ke paas selected class pakka hogi
         }
         //bottom cell selected or not
         let bottomSelected;
-        if(bottomCell){
+        if (bottomCell) {
             bottomSelected = bottomCell.hasClass("selected");
         }
-         //left cell selected or not
+        //left cell selected or not
         let leftSelected;
-        if(leftCell){
+        if (leftCell) {
             leftSelected = leftCell.hasClass("selected");
         }
-         //right cell selected or not
+        //right cell selected or not
         let rightSelected;
-        if(rightCell){
+        if (rightCell) {
             rightSelected = rightCell.hasClass("selected");
         }
 
-        if(topSelected){
+        if (topSelected) {
             $(ele).addClass("top-selected");
             topCell.addClass("bottom-selected");
         }
-       
-        if(bottomSelected){
+
+        if (bottomSelected) {
             $(ele).addClass("bottom-selected");
             bottomCell.addClass("top-selected");
         }
 
-        if(leftSelected){
+        if (leftSelected) {
             $(ele).addClass("left-selected");
             leftCell.addClass("right-selected");
         }
 
-        if(rightSelected){
+        if (rightSelected) {
             $(ele).addClass("right-selected");
             rightCell.addClass("left-selected");
         }
 
-    }else{
+    } else {
         $(".input-cell.selected").removeClass("selected top-selected bottom-selected left-selected right-selected");
     }
     $(ele).addClass("selected");
@@ -189,17 +189,21 @@ function selectCell(ele,e,topCell,bottomCell,leftCell,rightCell){
 let startcellSelected = false;
 let startCell = {};
 let endCell = {};
-
-$(".input-cell").mousemove(function(e){
+let scrollXRStarted = false;
+$(".input-cell").mousemove(function (e) {
     // console.log(e.buttons); /* Left click karke move karenge toh 1 print hoga and right click se 2 
     e.preventDefault(); // will prevent all the default functionalities of browser 
 
-    if(e.buttons == 1){
-        //$(".input-cell.selected").removeClass("selected top-selected bottom-selected left-selected right-selected");   
-        if(!startcellSelected){
+    if (e.buttons == 1) {
+        //$(".input-cell.selected").removeClass("selected top-selected bottom-selected left-selected right-selected");
+        if (e.pageX > ($(window).width() - 100) && !scrollXRStarted) { /*jab current window ke end pe ate hain toh scrollXR() trigger hota hai and scrollXRStrated start hota hai*/
+            scrollXR();
+        }
+        if (!startcellSelected) {
 
-            let [rowId,colId] = getRowCol(this);
-            startCell = {"rowId":rowId, "colId":colId};
+            let [rowId, colId] = getRowCol(this);
+            startCell = { "rowId": rowId, "colId": colId };
+            selectAllBetweenCells(startCell, startCell);
             startcellSelected = true;
             //console.log(startCell);  /* Give Start Cell row id and col id */
         }//else{
@@ -208,17 +212,17 @@ $(".input-cell").mousemove(function(e){
         //     selectAllBetweenCells(startCell,endCell); /* This function is cretaed below */
         // }
         // console.log(startCell,endCell);
-    }else{
+    } else {
         startcellSelected = false;
     }
 });
 
 /*Added mouse enter function coz it will help in reducing the loop done by mouse move */
-$(".input-cell").mouseenter(function(e){
-    if(e.buttons == 1){
-        let [rowId,colId] = getRowCol(this);
-        endCell = {"rowId":rowId, "colId":colId} 
-        selectAllBetweenCells(startCell,endCell); 
+$(".input-cell").mouseenter(function (e) {
+    if (e.buttons == 1) {
+        let [rowId, colId] = getRowCol(this);
+        endCell = { "rowId": rowId, "colId": colId }
+        selectAllBetweenCells(startCell, endCell);
     }
 })
 
@@ -226,13 +230,34 @@ $(".input-cell").mouseenter(function(e){
 here i can use 2 "for loop" concept, but before that need to find out wheather startcell is smaller or endcell. 
 coz to need to write for loop where it will loop from smaller id to larger id */
 
-function selectAllBetweenCells(start,end){
+function selectAllBetweenCells(start, end) {
     $(".input-cell.selected").removeClass("selected top-selected bottom-selected left-selected right-selected"); /*koi dusra select karne se pehle sabko remove kardo*/
-    for(let i = Math.min(start.rowId,end.rowId); i <= Math.max(start.rowId,end.rowId); i++){
-        for(let j = Math.min(start.colId,end.colId); j <= Math.max(start.colId,end.colId); j++){
+    for (let i = Math.min(start.rowId, end.rowId); i <= Math.max(start.rowId, end.rowId); i++) {
+        for (let j = Math.min(start.colId, end.colId); j <= Math.max(start.colId, end.colId); j++) {
             /* As already a function is written for selecting cell by name as selectCell, there need to pass ctrlKey manually in place of "e" */
-            let [topCell,bottomCell,leftCell,rightCell] = getTopLeftBottomRightCell(i,j);
-            selectCell($(`#row-${i}-col-${j}`)[0],{"ctrlKey": true},topCell,bottomCell,leftCell,rightCell) /* select a;; the cell that comes under start cell and end cell*/
+            let [topCell, bottomCell, leftCell, rightCell] = getTopLeftBottomRightCell(i, j);
+            selectCell($(`#row-${i}-col-${j}`)[0], { "ctrlKey": true }, topCell, bottomCell, leftCell, rightCell) /* select a;; the cell that comes under start cell and end cell*/
         }
     }
 }
+
+/* If we keep on dragging to the right by pressing left key, it should select the cell till the area we dragged */
+/* 1st How to know the width/max size of a window ?
+console.log($(window).width());  <=== JQuery
+console.log(e.pageX);  <=== Will give the max width on the X direction */
+
+/*Now will apply the scroll on dragging */
+
+let scrollXRInterval;
+function scrollXR() {  /* This function will take to the end of the window by using the interval continuously */
+    scrollXRStarted = true;
+    scrollXRInterval = setInterval(() => {
+        $("#cells").scrollLeft($("#cells").scrollLeft() + 100);
+    }, 100);
+}
+
+/* to stop in the middle while scrolling */
+$(".input-cell").mouseup(function (e) {
+    clearInterval(scrollXRInterval); /*stop the interval of scrolling */
+    scrollXRStarted = false; /* when stop dragging with left click, it will stop scrolling */
+})
